c0b2b3bd3d58dd3db5350a6ab90e9e4608faa501
MdAfsarHossain <afsar291722@gmail.com>
2026-09-20 16:10:42 +0600

    feat: add UpdateTweetDto and implement updateTweet method in TweetService; enhance TweetController to support tweet updates

--- src/tweet/tweet.controller.ts
+++ src/tweet/tweet.controller.ts
@@ -1,7 +1,8 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { TweetService } from './tweet.service';
 import { CreateTweetDto } from './dto/create-tweet.dto';
+import { UpdateTweetDto } from './dto/update-tweet.dto';
 
 @Controller('tweet')
 export class TweetController {
@@ -9,9 +10,26 @@ export class TweetController {
         private readonly tweetService: TweetService
     ){}
 
+    @Get()
+    public getAllUsersTweets() {
+        return this.tweetService.getAllUsersTweets()
+    }
+
+    // My All Tweets
+    @Get(':userId')
+    public getMyAllTweets(@Param('userId', ParseIntPipe) userId: number) {
+        return this.tweetService.getMyAllTweets(userId);
+    }
+
     @Post()
     public createTweet(@Body() tweet: CreateTweetDto) {
         return this.tweetService.createTweet(tweet)
+    }
+
+    // Update Tweet
+    @Patch()
+    public updateTweet(@Body() tweet: UpdateTweetDto) {
+        return this.tweetService.updateTweet(tweet);
     }
 
 }
