5493b21159c39497b9c1d63cc1c9921f8e358764
MdAfsarHossain <afsar291722@gmail.com>
2026-09-21 22:41:40 +0600

    feat: add soft delete functionality for hashtags in HashtagService and HashtagController

--- src/hashtag/hashtag.entity.ts
+++ src/hashtag/hashtag.entity.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { Tweet } from "src/tweet/tweet.entity";
-import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, DeleteDateColumn, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Hashtag {
@@ -13,6 +13,9 @@ export class Hashtag {
         unique: true
     })
     name: string;
+
+    @DeleteDateColumn()
+    deletedAt: Date;
 
     @ManyToMany(() => Tweet, (tweet) => tweet.hashtags, {onDelete: 'CASCADE'})
     tweets: Tweet[]
